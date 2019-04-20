c369dca6dc95594555a41d4da6bd6253d4cc4789
pmadlife <49767365+pmadlife@users.noreply.github.com>
2019-04-20 12:03:33 +0100

    Add files via upload

--- Index.java
+++ Index.java
@@ -14,34 +14,42 @@ public class Index {
 		switch(choice) {
 			case 1 :
 			  Lisa.main(new String[0]);
+			  System.out.println();
 			break;
 			case 2 :
 					  BigNumber.main(new String[0]);
+					  System.out.println();
 			break;
 			case 3 :
 					  Number.main(new String[0]);
+					  System.out.println();
 			break;
 			case 4 :
 					  Two.main(new String[0]);
+					  System.out.println();
 			break;
 			case 5 :
 					  NameAge.main(new String[0]);
+					  System.out.println();
 			break;
 			case 6 :
 					  ForLoop.main(new String[0]);
+					  System.out.println();
 			break;
 			case 7 :
 					  Months.main(new String[0]);
+					  System.out.println();
 			break;
 			case 8 :
 					  Cards.main(new String[0]);
+					  System.out.println();
 			break;
 			default:
-				System.out.println("Please enter a valid choice. < From 1 to 8 >");
+				System.out.println("Please enter a valid choice. < From 1 to 8 >\n");
 			break;
 		}
 
-		System.out.println("Menu:\n1. Lisa\n2. Big Number\n3. Number\n4. Two\n5. Name Age\n6. For Loop\n7. Months\n 8. Cards ");
+		System.out.println("Menu:\n1. Lisa\n2. Big Number\n3. Number\n4. Two\n5. Name Age\n6. For Loop\n7. Months\n 8. Cards \n");
 		System.out.print("Which program do you want to run <Ctrl+Z to quit.>: ");
  	    choice = input.nextInt();
 
